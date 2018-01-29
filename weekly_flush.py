ed0ac410becce501f5b7d260cc3c238c40626f5d
Dhaval Savalia <dhaval.savalia6@gmail.com>
2018-01-29 09:19:12 +0530

    add weekly flush script

---
+++ weekly_flush.py
@@ -0,0 +1,17 @@
+import os, django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polls.settings')
+django.setup()
+from django.contrib.auth.models import User
+from mainapp.models import Profile
+
+list_users = list(User.objects.order_by('-pk'))
+
+def flush_all():
+	for n_user in list_users:
+		model_con = Profile.objects.get(user=n_user)
+		model_con.has_voted = False
+		model_con.save()
+
+flush_all()
+print('ALL RECORDS FLUSHED!!!')
