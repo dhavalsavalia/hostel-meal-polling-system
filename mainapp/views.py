629573aa7a9ad089865fc70f036f800e323e347a
Dhaval Savalia <dhaval.savalia6@gmail.com>
2018-04-28 02:00:10 +0530

    clean code

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
 from .models import Choice, Profile
 
 @login_required
@@ -10,28 +9,19 @@ def index(request):
 	user_v = None
 	get_choices = Choice.objects.values('choices', 'vote', 'users')
 	get_profile = Profile.objects.values('user', 'has_voted')
-	
 	new_profile = list(get_profile)
-
 	new_choices = list(get_choices)
-
 	for p in new_profile:
 		user_p      = p['user']
 		has_voted_p = p['has_voted']
-		
 		if user_p == request.user.id and has_voted_p == False:
 			if food:
 				model_con = Profile.objects.get(user=request.user.id)
 				model_con.has_voted = True
 				model_con.save()
-
 				for q in new_choices:
 					choice   = q['choices']
 					vote_v   = q['vote']
-
-					print(vote_v)
-
-
 					if food:
 						if food == choice:
 							vote_v += 1
@@ -39,9 +29,7 @@ def index(request):
 							model_conn.vote = vote_v
 							model_conn.users.add(request.user)
 							model_conn.save()
-
 				return render(request, 'thanks.html', {})
-
 		if user_p == request.user.id and has_voted_p == True:
 			context = dict()
 			vote_f = 0
@@ -51,14 +39,5 @@ def index(request):
 			order_items = Choice.objects.order_by('-vote')
 			context['order_items'] = order_items
 			context['vote_f'] = vote_f
-
-
-
 			return render(request, 'already_done.html', context)
-
-
-
 	return render(request, 'index.html', {})
-
-
-
